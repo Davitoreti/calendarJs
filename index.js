203556951fa0074a69a901a43cac970c1a2f59ac
Davi Toreti Scepaniuk <0341907@adm.feevale.br>
2024-07-24 18:28:37 -0300

    developing the project

--- index.js
+++ index.js
@@ -3,22 +3,49 @@ const currentDay = currentDate.getDate();
 const currentMonth = currentDate.getMonth() + 1;
 const currentYear = currentDate.getFullYear();
 
+let days = [];
+let months = [];
+let years = [];
+let numbers = 1;
+let lastDays = []
+
 function generateCalendar() {
     const table = document.getElementById('table');
-    const month = document.getElementById('month');
-    const day = document.getElementById('cell');
+    const day = document.getElementsByTagName('td');
 
-    let days = [];
-    let number = 0;
-
-   day.forEach(function(day){
-    for (let i = 0; i <= 31; i++) {
-        console.log(i);
+    function makeDays() {
+      for (let i = 0; i <= 34; i++) {
+        day[i].innerText = numbers++;
+        if(numbers > 31){
+          numbers = 0;
+          numbers++;
+        } 
       }
-   })
-    
-   console.log(day)
-    
+      days.push(day);
+        return day;
+    }
+
+    function makeMonths() {
+      const month = days;
+      for (let i = 0; i < 12; i++) {
+        month[i]
+        months.push(month)
+      }
+      return months;
+    };
+
+    function makeYear() {
+      const year = months;
+      for (let i = 0; i < 5; i++) {
+        year[i]
+        years.push(year)
+      }
+      return years
+    };
+  
+    console.log(makeDays());
+    // console.log(makeMonths())
+    // console.log(makeYear())  
 }
 
 console.log(generateCalendar());
